1ba14b11155dabc48e1194db2be7865508b44fa4
Carlos Diez <cdiez@lksnext.com>
2025-01-21 17:23:55 +0100

    feat: Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # ConnectorEventLog
 
 This is a server that receives Connector events and logs them.
+
+## Requeriments
+
+```bash
+pip install "fastapi[standard]"
+```
+
+## Run
+
+Navigate to the root of the project and run the following command:
+
+```bash
+fastapi dev main.py
+```
---
+++ main.py
@@ -0,0 +1,38 @@
+from typing import Annotated
+
+from fastapi import Request, FastAPI, Header
+
+app = FastAPI()
+
+logs = []
+
+class event:
+    def __init__(self, ce_time, ce_type, ce_source, ce_id, ce_specversion, body):
+        self.ce_time = ce_time
+        self.ce_type = ce_type
+        self.ce_source = ce_source
+        self.ce_id = ce_id
+        self.ce_specversion = ce_specversion
+        self.body = body
+
+
+@app.get("/event")
+async def getNewEvent(request: Request, 
+                      ce_time: Annotated[str | None, Header()] = None,
+                      ce_type: Annotated[str | None, Header()] = None,
+                      ce_source: Annotated[str | None, Header()] = None,
+                      ce_id: Annotated[str | None, Header()] = None,
+                      ce_specversion: Annotated[str | None, Header()] = None):
+    print(ce_time)
+    print(ce_type)
+    print(ce_source)
+    print(ce_id)
+    print(ce_specversion)
+    body = await request.json()
+    print(body)
+    logs.append(event(ce_time, ce_type, ce_source, ce_id, ce_specversion, body))
+    return "204"
+
+@app.get("/logs")
+async def getLog():
+    return logs
